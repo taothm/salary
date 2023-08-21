b31a566c2ea55fc70daea012707fd4814731de21
taothm <142779642+taothm@users.noreply.github.com>
2023-08-21 14:46:02 +0700

    footer.js

---
+++ footer.js
@@ -0,0 +1,4 @@
+function setFooter(){
+  document.querySelector('.copyright').innerHTML = 
+    '<center style="font-size:12px;">Copyright &copy; 2023 Design:<a style="color:gray;text-decoration:none" href="https://www.facebook.com/Taothm/" target="_blank"> Tao_thm</a></center><center style="font-size:12px;">Credit:<a style="color:gray;text-decoration:none" href="https://examblog64.krooluang.com/" target="_blank"> ExamVip</a></center>'
+}
